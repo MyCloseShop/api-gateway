ffe0de29a962ddb778ec4786e81af9abba343c6a
amara_s <amara_s@etna-alternance.net>
2025-07-15 16:35:16 +0200

    fix: update api-gateway image version and refactor HomeController to serve HTML content

--- src/main/java/com/etna/gpe/mycloseshop/api_gateway/controllers/HomeController.java
+++ src/main/java/com/etna/gpe/mycloseshop/api_gateway/controllers/HomeController.java
@@ -1,13 +1,20 @@
 package com.etna.gpe.mycloseshop.api_gateway.controllers;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.http.MediaType;
+import org.springframework.util.StreamUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+@RestController
 public class HomeController {
-    @GetMapping("/")
-    public String home() {
-        return "forward:/index.html";
+
+    @GetMapping(value = "/", produces = MediaType.TEXT_HTML_VALUE)
+    public String home() throws IOException {
+        ClassPathResource resource = new ClassPathResource("static/index.html");
+        return StreamUtils.copyToString(resource.getInputStream(), StandardCharsets.UTF_8);
     }
 }
-
